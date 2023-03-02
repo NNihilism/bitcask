da6c1ca60593f17cdc7871c04e07265e898c89be
NNihilism <405307400@qq.com>
2023-03-02 14:39:24 +0800

    删除无效注解

--- internal/bitcask_master_slaves/idl/proxy.thrift
+++ internal/bitcask_master_slaves/idl/proxy.thrift
@@ -1,12 +1,22 @@
 namespace go prxyService
 
+struct BaseResp {
+    1: i64 status_code
+    2: string status_message
+    3: i64 service_time
+}
+
+struct LogEntry {
+    1: string key
+    2: string value
+    3: i64 score
+    4: i64 expireAt
+}
+
 
 # 数据操作，即数据的增删改查（被请求方将会被写入数据）
 struct LogEntryRequest {
     1: i64 entry_id // 客户端发起的请求不会有这个标识，主节点发送请求时会有这个标识，方便进度同步
-    # 2: OperationCode opCode
-    # 3: LogEntry entry
-    # TODO k/v带空格怎么办? 在解析args时不对""环绕内容进行split?
     2: string cmd  
     3: list<string> args
     4: string master_id
@@ -23,4 +33,6 @@ service ProxyService {
     bool Ping(1: bool req)
 
     LogEntryResponse OpLogEntry(1: LogEntryRequest req)
+
+    bool Proxy(1: string master_addr)
 }
